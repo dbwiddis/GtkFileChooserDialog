dd0e02a73928aa28740d8d0cacbfadb848cff57a
Daniel Widdis <widdis@gmail.com>
2020-11-15 02:57:16 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GtkFileChooserDialog
+
+Implementation of GtkFileChooserDialog in JNA
---
+++ src/main/java/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+import gtk.jna.Gtk3;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Gtk3 GTK = Gtk3.INSTANCE;
+    }
+
+}
